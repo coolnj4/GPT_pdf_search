6720fb78da64dc1fbbffe1c9822ebcdf039fad0b
coolnj4 <98026895+coolnj4@users.noreply.github.com>
2023-09-22 11:32:04 +0530

    Update Backend_logic.py

--- Backend_logic.py
+++ Backend_logic.py
@@ -38,7 +38,7 @@ def get_vectorstore(text_chunks):
     return vectorstore
 
 
-def get_conversation_chain(vectorstore):
+def conversation_chain(vectorstore):
     llm = ChatOpenAI()
     # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
 
